bbc7884bb3bb1ea75801531e61d0ffc416fb28c5
Rankin <srankin@gpstrategies.com>
2017-06-26 13:58:55 -0400

    added partition structure

--- app.js
+++ app.js
@@ -10,15 +10,24 @@ app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.render('home');
+  var params = {
+    css: 'public/styles/home_style.css',
+    summary_title: 'Neo4J doesnt work',
+    summary_thread: 'Graphing DBs',
+    summary_user: 'Barak Insane Yomama',
+    summary_content: 'ntsdoih sdfnlsd sdfnsnd sdfnsdf sdfkln sdfnloier',
+    summary_date: 'June 43rd, 1',
+    summary_comments: '-1'
+  };
+  res.render('home', params);
 });
 
 app.get('/signup', function(req, res) {
-  res.render('signup');
+  res.render('signup', {css: 'public/styles/signup_style.css'});
 });
 
 app.get('/login', function(req, res) {
-  res.render('login');
+  res.render('login', {css: 'public/styles/login_style.css'});
 });
 
 app.listen(3000, function() { console.log('listening on port 3000'); });
