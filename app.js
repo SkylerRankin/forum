2a94cae8065fb61f9aa247d2e50df4a0ee769e49
Rankin <srankin@gpstrategies.com>
2017-06-27 15:00:44 -0400

    started mongo integration

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var ejs = require('ejs');
 var path = require('path');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var app = express();
+mongoose.connect('mongodb://localhost:27017/test');
 
 app.use('/public', express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   var params = {
+    loggedin: false,
     css: 'public/styles/home_style.css',
     summary_title: 'Neo4J doesnt work',
     summary_thread: 'Graphing DBs',
@@ -24,6 +31,10 @@ app.get('/', function(req, res) {
 
 app.get('/signup', function(req, res) {
   res.render('signup', {css: 'public/styles/signup_style.css'});
+});
+
+app.post('/signup', function(req, res) {
+  console.log(req.body.first_name + ' ' + req.body.last_name + ' - ' + req.body.email);
 });
 
 app.get('/login', function(req, res) {
---
+++ models/user.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+var userSchema = mongoose.Schema({
+  username: { type: String, require: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  email: { type: String, required: true, unique: true }
+});
+
+userSchema.methods.getUsername = function() { return this.username; };
+userSchema.methods.getEmail = function() { return this.email; };
+userSchema.methods.getDateCreated = function() { return this.createdAt; };
+userSchema.methods.checkPassword = function(pass, done) {
+  bcrypt.compare(pass, this.password, function(err, match) {
+    done(err, match);
+  });
+}
+
+userSchema.pre('save', function(done) {
+  var user = this;
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return done(err); }
+    bcrypt.hash(user.password, salt, function() {}, function (err, hashed) {
+      if (err) {return done(err);}\
+      user.password = hashed;
+      done();
+    });
+  });
+});
+
+var User = mongoose.model('User', userSchema);
+modeule.exports = User;
